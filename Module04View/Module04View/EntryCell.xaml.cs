67a0f70b0adfd37753cf54aedb860851c02ae274
Rich1068 <rs106848@gmail.com>
2024-03-07 13:01:23 +0800

    2nd commit

--- Module04View/Module04View/EntryCell.xaml.cs
+++ Module04View/Module04View/EntryCell.xaml.cs
@@ -16,5 +16,12 @@ namespace Module04View
         {
             InitializeComponent();
         }
+        void OnSubmitEntry(object sender, EventArgs e)
+        {
+            string entry1Text = Title.Text;
+            string entry2Text = Description.Text;
+            string entry3Text = email.Text;
+            DisplayAlert("Enter Values", $"Title: {entry1Text}\nDescription: {entry2Text}\nEmail: {entry3Text}\n are you sure you want to submit?", "Ok", "Cancel");
+        }
     }
 }
--- Module04View/Module04View/ImageCell.xaml.cs
+++ Module04View/Module04View/ImageCell.xaml.cs
@@ -21,11 +21,11 @@ namespace Module04View
 
             ExampleView.ItemsSource = examples;
 
-            examples.Add(new Example { Title = "Example 1", Description = "Description 1", Image = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/83240012-8303-4d4b-abac-4176896a795d/dgkr9w8-df64af14-e6a1-4d55-a467-39a2577c6403.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzgzMjQwMDEyLTgzMDMtNGQ0Yi1hYmFjLTQxNzY4OTZhNzk1ZFwvZGdrcjl3OC1kZjY0YWYxNC1lNmExLTRkNTUtYTQ2Ny0zOWEyNTc3YzY0MDMucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.Yyd9mvUlSM_1eajXcIEolFltYbijAuiDtDqkQcwP_Qg" });
-            examples.Add(new Example { Title = "Example 2", Description = "Description 2", Image = "" });
-            examples.Add(new Example { Title = "Example 3", Description = "Description 3", Image = "" });
-            examples.Add(new Example { Title = "Example 4", Description = "Description 4", Image = "" });
-            examples.Add(new Example { Title = "Example 5", Description = "Description 5", Image = "" });
+            examples.Add(new Example { Title = "Example 1", Description = "Description 1", Image = "https://64.media.tumblr.com/15c6297baeb5f0c32a790c85283c7881/78b263366d7c367c-c6/s1280x1920/73d5c784586714f68f77de1f2073282f05562702.png" });
+            examples.Add(new Example { Title = "Example 2", Description = "Description 2", Image = "https://static.wikia.nocookie.net/houkai-star-rail/images/c/c7/Jing_Yuan_Sticker_02.png/revision/latest?cb=20230420194038" });
+            examples.Add(new Example { Title = "Example 3", Description = "Description 3", Image = "https://64.media.tumblr.com/1ac4e3703c2011cec19e8113223e4fb9/78b263366d7c367c-45/s1280x1920/9b13a37406db015b4f7f0ed63a32adb9e617d2a1.png" });
+            examples.Add(new Example { Title = "Example 4", Description = "Description 4", Image = "https://64.media.tumblr.com/775c6e14d485eaf7509406c1595d864f/78b263366d7c367c-ef/s1280x1920/f5bb6df943143e347a2f5cb70c6d2822728c43e4.png" });
+            examples.Add(new Example { Title = "Example 5", Description = "Description 5", Image = "https://64.media.tumblr.com/3bdfc7471d7f23ad4ecb1aee89751915/78b263366d7c367c-90/s1280x1920/a57e62ff54d4766f28e70590165f63678f5b14d8.png" });
         }
     }
 }
